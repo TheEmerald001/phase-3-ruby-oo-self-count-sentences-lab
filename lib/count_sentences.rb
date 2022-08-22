8c437da847c29cd30d2fad802aa5e79443debb43
steve <austines811@gmail.com>
2022-08-23 01:38:25 +0300

    commit tree b1a7427546962db7535289d1e7ea99e8c4f7584d

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
+
+    #Above is the refactored code below using the ternary in Ruby
+    # if 
+    #   self.end_with?(".")
+    #   true
+    # else
+    #   false
+    # end
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).filter { |sentence| !sentence.empty? }.size
   end
 end
